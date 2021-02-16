f4627cbd99f0ac4ad7e4c955d2419ca7b9663f84
Taylor Covington <tcovingtondev@gmail.com>
2021-02-15 19:54:24 -0700

    connected to backend using graphql, getting product data, styled

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,18 @@
 import Link from 'next/link';
+import styled from 'styled-components'
+
+const NavStyles = styled.nav`
+  background-color: var(--gray);
+    padding: 1rem 0;
+`;
 
 export default function Nav() {
-  return <nav>
+  return <NavStyles>
     <Link href="/">HOME</Link>
     <Link href="/blog">BLOG</Link>
     <Link href="/resources">RESOURCES</Link>
     <Link href="/about">ABOUT</Link>
     <Link href="/work">WORK WITH ME</Link>
-  </nav>
+    <Link href="/products">PRODUCTS</Link>
+  </NavStyles>
 }
--- components/styles/PriceTag.js
+++ components/styles/PriceTag.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 const PriceTag = styled.span`
-  background: var(--red);
-  transform: rotate(3deg);
+  background: var(--revBlue);
+  /* transform: rotate(3deg); */
   color: white;
   font-weight: 600;
   padding: 5px;
   line-height: 1;
-  font-size: 3rem;
+  font-size: 2rem;
   display: inline-block;
   position: absolute;
   top: -3px;
